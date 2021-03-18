86f8a667cc2a8b707748a4ff21608a761a0fc191
Maat Gomes <mateusgomesgo@hotmail.com>
2021-03-18 18:01:57 -0300

    yes

--- src/main/java/com/maats/saveseries/controller/SeriesController.java
+++ src/main/java/com/maats/saveseries/controller/SeriesController.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -57,11 +56,13 @@ public class SeriesController {
     }
 
     @GetMapping("/cardDetails/{id}")
-    public ModelAndView getCardDetails(@PathVariable("id") Long id){
-        ModelAndView mv = new ModelAndView("cardDetails");
-        Serie serie = serieService.findById(id);
-        mv.addObject("serie", serie);
-        return mv;
+    public String getCardDetails(@PathVariable("id") Long id) {
+
+        serieService.findById(id);
+
+        return "series";
     }
+  
+
 
 }
